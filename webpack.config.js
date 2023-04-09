32edd4bed73a792d8f868809a4efaff06bde32a5
alaverArt <alaverArt@yandex.ru>
2023-04-09 18:09:09 +0300

    Webpack fixes - some webpack configs - moving assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,81 @@
 const path = require('path');
 const miniCss = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+const isDevMode = mode === 'development';
+const target = isDevMode ? 'web' : 'browserslist';
+const devtool = isDevMode ? 'source-map' : undefined;
+
 module.exports = {
-    mode: 'development',
+    mode,
+    target,
+    devtool,
     entry: './src/app.js',
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        filename: 'bundle.[fullhash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     module: {
-        rules: [{
-            test:/\.(s*)css$/,
-            use: [
-            miniCss.loader,
-            'css-loader',
-            'sass-loader',
-            ]
-        }]
+        rules: [
+            {
+                test:/\.(s*)css$/,
+                use: [
+                    miniCss.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [require('postcss-preset-env')],
+                            },
+                        },
+                    },
+                    'sass-loader',
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: 'html-loader',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name][ext]',
+                },
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext]',
+                },
+            },
+        ]
     },
     plugins: [
         new miniCss({
-            filename: 'style.css',
+            filename: 'style.[fullhash].css',
         }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'index.html'),
+        })
     ],
+    optimization: {
+        minimizer: [
+            `...`,
+            new CssMinimizerPlugin(),
+        ],
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src/')
+        },
+    },
     devServer: {  // configuration for webpack-dev-server
-        static: {
-            directory: __dirname + '/dist',
-        },  //source of static assets
         port: 8080, // port to run dev-server
     }
 };
